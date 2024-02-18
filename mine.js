543c000d266fda1f9d2d595b66c40d81b34e150b
Afeez Adesina <afeezadesinaa@gmail.com>
2024-02-18 23:29:01 +0100

    update mine.js

--- mine.js
+++ mine.js
@@ -1,11 +1,38 @@
-str = 'adesina afeez'
+let str = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+let afeez = str.reduce((acc, curr) => {
+    return me(mey) => (acc + curr) )};
+console.log((afeez)(me));
 
-// const toUpperCase = str => str.toUpperCase()
 
-const name = function toUpperCase(str){
-        return str.toUpperCase()
+let number = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+let okay = number.reduce((acc, curr) => { 
+    return me() => {
+        return acc + curr;
     }
+});
+console.log(okay());
 
-console.log(name(str))
+let user = {
+    firstName: "Ilya",
+    sayHi() {
+        let arrow = () => alert(this.firstName);
+        arrow();
+    }
+};
+user.sayHi(); 
+  
+let mine = {
+    a: 20,
+    b: 30,
+    add() {
+        return this.a + this.b;
+    }
+};
+mine.add(); // 50
 
-toLowerCase = str => str.toLowerCase();
+let ayo = {
+    ...mine,
+    a=undefined,
+    b=undefined
+};
+console.log(ayo.add.call((),{a:2,b:3})); // NaN
