32cad398ea6a33bb13593a2d93b85ed9a1e3d1c6
Afeez Adesina <afeezadesinaa@gmail.com>
2024-02-21 18:53:17 +0100

    update

--- call-apply-bind.js
+++ call-apply-bind.js
@@ -77,3 +77,5 @@ NodeList.prototype.on = NodeList.prototype.addEventListener = function (name, fn
     elem.on(name, fn);
   });
 }
+
+
--- mine.js
+++ mine.js
@@ -1,38 +1,12 @@
-let str = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-let afeez = str.reduce((acc, curr) => {
-    return me(mey) => (acc + curr) )};
-console.log((afeez)(me));
+arrayOfNumbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+let afeez = arrayOfNumbers.reduce((acc, curr) => acc + curr, 0);
+console.log(afeez);
 
 
-let number = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-let okay = number.reduce((acc, curr) => { 
-    return me() => {
+numbers = [2, 3, 4, 5, 6, 7, 8, 9, 10];
+let newNumbers = numbers.reduce((acc, curr) => { 
+    return mine = () => { 
         return acc + curr;
     }
-});
-console.log(okay());
-
-let user = {
-    firstName: "Ilya",
-    sayHi() {
-        let arrow = () => alert(this.firstName);
-        arrow();
-    }
-};
-user.sayHi(); 
-  
-let mine = {
-    a: 20,
-    b: 30,
-    add() {
-        return this.a + this.b;
-    }
-};
-mine.add(); // 50
-
-let ayo = {
-    ...mine,
-    a=undefined,
-    b=undefined
-};
-console.log(ayo.add.call({a:2,b:3}),); // NaN
+})
+console.log(newNumbers(mine)); 
